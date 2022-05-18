10f73e89b987d9d4da29846e214335e7a3eb069c
Patryk "ArixElo" Zimnicki <36364890+ArixElo@users.noreply.github.com>
2022-05-18 08:51:33 +0200

    fixup: remove conflict brackets
    I hate doing rebase being asleep

--- aosp_renoir.mk
+++ aosp_renoir.mk
@@ -11,29 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-<<<<<<<< HEAD:lineage_renoir.mk
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-========
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
->>>>>>>> b918813 (renoir: PixelExperience bringup):aosp_renoir.mk
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-<<<<<<<< HEAD:lineage_renoir.mk
-PRODUCT_NAME := lineage_renoir
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-========
 PRODUCT_NAME := aosp_renoir
-
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
->>>>>>>> b918813 (renoir: PixelExperience bringup):aosp_renoir.mk
